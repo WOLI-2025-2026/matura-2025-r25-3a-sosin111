289c0c6a11f84c5c06ed4c311550c19991df46f4
sosin111 <franciszek.sosinski@woli.edu.pl>
2025-11-03 15:25:04 +0000

    sosek

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 //Franciszek Sosiński
 
 #include <iostream>
-int main() {
-    std::cout << "Hello, Matura 2025!\n";
-    return 0;
+
+using namespace std;
+
+string z2_1_path_symbole_txt = "../zalacniki-2025/symbole.txt";
+
+bool czy_palindrom (string tekst){
+    int ile_znakow= tekst.length();
+    for(int i=0; i<ile_znakow/2 ; i++){
+        if (tekst [i] != tekst[ile_znakow-i-1])
+        return false;
+    }
+}
+
+void zadanie_1(){
+    ifstream plik(z2_1_path_symbole_txt);
+    if(!plik.is_open()){
+        crr << "Nie mozna otworzyc pliku"
+    }
+
+    string linia;
+    while(plik>linia){
+        cout<<linia << endl; 
+    }
+
+    plik.cole();
+}
+
+int main(){
+
+
 }
