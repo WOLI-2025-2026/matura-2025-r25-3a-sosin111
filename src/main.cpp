ddf5fcdc710ef8d19a032c69fadeb71bd6e2fe9a
sosin111 <franciszek.sosinski@woli.edu.pl>
2025-11-13 09:29:01 +0000

    sosek

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,39 @@
 
 using namespace std;
 
-string z2_1_path_symbole_txt = "../zalacniki-2025/symbole.txt";
+string z2_1_path_symbole_txt = "/workspaces/matura-2025-r25-3a-sosin111/zalaczniki-2025/symbole.txt";
 
-// Funkcja sprawdzająca, czy tekst jest palindromem
 bool czy_palindrom(string tekst) {
     int ile_znakow = tekst.length();
     for (int i = 0; i < ile_znakow / 2; i++) {
         if (tekst[i] != tekst[ile_znakow - i - 1])
             return false;
     }
-    return true; // brakowało return true
+    return true; 
 }
 
 void zadanie_1() {
     ifstream plik(z2_1_path_symbole_txt);
     if (!plik.is_open()) {
         cerr << "Nie mozna otworzyc pliku" << endl;
-        return; // zakończ funkcję, jeśli nie udało się otworzyć pliku
+        return; 
     }
+
+    ofstream wynik("wynik2_1.txt");
 
     string linia;
-    while (getline(plik, linia)) { // poprawiono warunek czytania
+    while (plik >> linia) { 
+       if(czy_palindrom(linia)){
         cout << linia << endl;
+        wynik << linia << endl;
     }
-
-    plik.close(); // poprawiono literówkę (było "cole")
+}
+    wynik.close();
+    plik.close(); 
 }
 
 int main() {
-    zadanie_1(); // wywołanie funkcji testowej
-
-    // Test działania funkcji czy_palindrom
-    cout << (czy_palindrom("kajak") ? "Palindrom" : "Nie palindrom") << endl;
-    cout << (czy_palindrom("test") ? "Palindrom" : "Nie palindrom") << endl;
+    zadanie_1(); 
 
     return 0;
-}g
+}
